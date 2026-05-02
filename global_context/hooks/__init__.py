125c488a2eb88ec905e9ddecd378e5dfdade2949
Fernando Celmer <email@fernandocelmer.com>
2026-05-02 00:35:29 -0300

    ⚙️ FEATURE: Add SessionStart and UserPromptSubmit recall hooks

--- global_context/hooks/__init__.py
+++ global_context/hooks/__init__.py
@@ -1,3 +1,4 @@
 from global_context.hooks.autosave import autosave_transcript
+from global_context.hooks.recall import session_start, user_prompt_submit
 
-__all__ = ["autosave_transcript"]
+__all__ = ["autosave_transcript", "session_start", "user_prompt_submit"]
